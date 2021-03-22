e4d41d666217ce87a050430909ad43c2142749cf
ramces batista peña <ramcesbape@gmail.com>
2021-03-22 01:02:13 -0400

    Entrada Ejercicio-2

---
+++ Ejercicio-2/Contacto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_2
+{
+    public class Contacto
+    {
+
+        public string nombre;
+        public string apellido;
+        public string telefono;
+        public string direccion;
+
+
+        public Contacto()
+        {
+
+            nombre = "";
+            apellido = "";
+            telefono = "";
+            direccion = "";
+        }
+        public void InNombre(String n)
+        {
+            nombre += n;
+        }
+        public void InApellido(String a)
+        {
+            apellido += a;
+        }
+        public void InTelefono(String t)
+        {
+            telefono += t;
+        }
+        public void InDireccion(String d)
+        {
+           direccion += d;
+        }
+        public void SetContacto()
+        {
+            Console.WriteLine("Digite su nombre");
+            InNombre(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Digite su apellido");
+            InApellido(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Digite su numero de telefono");
+            InTelefono(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Digite su direccion");
+            InDireccion(Console.ReadLine());
+            Console.Clear();
+
+
+        }
+        public void Saludar()
+        {
+            Console.WriteLine("Hola, soy {0} {1}, mi direccion es: {2} y mi numero de telefono es: {3}", nombre, apellido, direccion, telefono );
+
+        }
+
+    }
+}
---
+++ Ejercicio-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        class ProbarContacto
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Programa desarrollado por Ramces Batista Peña para introdicir datos de contactos ");
+                Contacto Con = new Contacto();
+                Contacto Con1 = new Contacto();
+                Console.WriteLine("Introdusca los datos del contacto 1");
+                Con.SetContacto();
+                Console.WriteLine("Introdusca los datos del contacto 2");
+                Con1.SetContacto();
+                Console.WriteLine("Contacto 1");
+                Con.Saludar();
+                Console.WriteLine("Presione enter para continuar ");
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("Contacto 2");
+                Con1.Saludar();
+                Console.WriteLine("Presione enter para continuar ");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+        }
+    }
+}
